5485566a7ba2011ac9d525c86d57ea993041eecf
castilloleogithub <castillo.leo@gmail.com>
2022-08-05 16:16:59 -0300

    inicio de backend
    spring inicial

---
+++ Backend/src/main/java/com/porfolio/lcastillo/LcastilloApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.lcastillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcastilloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcastilloApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/porfolio/lcastillo/LcastilloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.lcastillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcastilloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
